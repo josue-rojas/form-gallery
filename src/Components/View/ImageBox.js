3cfd2a7ffed7941ead827d31f4c011e18cabf602
josue rojas <josuerojas.rojas@gmail.com>
2018-03-29 14:28:22 -0400

    simple loader for images added

--- src/Components/View/ImageBox.js
+++ src/Components/View/ImageBox.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import './Styles/ImageBox.css'
+import './Styles/ImageBox.css';
+import Gradient from '../Loaders/Gradient';
 
 export default class ImageBox extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      loaded: false,
+    }
     this.handleClick = this.handleClick.bind(this);
+    // this.handleImageLoad = this.handleImageLoad.bind(this);
   }
 
   handleClick(){
     this.props.handleClick(this.props.imageLink, this.props.title, this.props.description)
   }
-
+  // sorta of a trick for background img loaded https://jsfiddle.net/tovic/gmzSG/
+  componentDidMount(){
+    const temp_img = new Image();
+    temp_img.onload = ()=>{
+      this.setState({loaded: true})
+    }
+    temp_img.src = this.props.imageLink
+  }
   render() {
     const style = {
       image: {
@@ -19,7 +31,14 @@ export default class ImageBox extends Component {
         backgroundPosition: 'center',
         position: 'relative',
         height: '300px',
-        float: 'left'
+        float: 'left',
+      },
+      loaderWrapper: {
+        display: this.state.loaded ? 'none' : 'flex',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
       },
       hover: {
         position: 'absolute',
@@ -34,7 +53,14 @@ export default class ImageBox extends Component {
       }
     }
     return(
-      <div className='imageBox' style={style.image} onClick={this.handleClick}>
+      <div className={'imageBox ' + (this.state.loaded ? 'loaded' : '')} style={style.image} onClick={this.handleClick}>
+        <div style={style.loaderWrapper}>
+          <Gradient
+            width={'100%'}
+            height={'100%'}
+            colors={'rgba(95, 95, 95, 0.5), rgba(255, 255, 255, 0.14), rgba(95, 95, 95, 0.5)'}
+            delay={4000}/>
+        </div>
         <div style={style.hover} className='imageBox-hover'>
           {this.props.title}
         </div>
